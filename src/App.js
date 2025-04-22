0cac75744bc64b953847f612be8516bba5f50997
haradeep <haradeeps119@gmail.com>
2025-04-22 12:40:33 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -12,23 +12,7 @@ function App() {
 
   return (
     <Router>
-      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <Link className="navbar-brand" to="/">Placement Checker</Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
-              {isLoggedIn && (
-                <li className="nav-item"><Link className="nav-link" to="/eligibility">Eligibility</Link></li>
-              )}
-            </ul>
-          </div>
-        </div>
-      </nav> */}
+      
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
           <Link className="navbar-brand" to="/">Placement Checker</Link>
